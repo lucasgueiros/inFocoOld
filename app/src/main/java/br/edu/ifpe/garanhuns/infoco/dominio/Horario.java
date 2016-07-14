698e5f816dc1284275bd68d3099a6b8e463782fe
Lucas Gueiros <lucasdantasgueiros@gmail.com>
2016-07-14 13:12:33 -0300

    Removendo classe DiaDaSemana = agora esta dentro de Horario

--- app/src/main/java/br/edu/ifpe/garanhuns/infoco/dominio/Horario.java
+++ app/src/main/java/br/edu/ifpe/garanhuns/infoco/dominio/Horario.java
@@ -109,4 +109,23 @@ public class Horario {
                 ", minuto=" + minuto +
                 '}';
     }
+
+    /**
+     * Created by lucas on 09/07/16.
+     */
+    public enum DiaDaSemana {
+        DOMINGO(1), SEGUNDA(2), TERCA(3), QUARTA(4), QUINTA(5), SEXTA(6), SABADO(7);
+
+        // segue o padrão do Calendar https://developer.android.com/reference/java/util/Calendar.html
+        // o dia contando o domingo como 1o, a segunda como 2o etc
+        private int dia;
+
+        DiaDaSemana(int dia) {
+            this.dia = dia;
+        }
+
+        public int getDia() {
+            return dia;
+        }
+    }
 }
